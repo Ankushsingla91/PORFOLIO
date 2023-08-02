4e6cb22d41de56170e67a24cb64a5ecaa83b9c50
Ankushsingla91 <111479407+Ankushsingla91@users.noreply.github.com>
2023-08-02 17:37:43 +0530

    Add files via upload

---
+++ WorkCardData.js
@@ -0,0 +1,25 @@
+import pro1 from "../assets/project1.png"
+import pro2 from "../assets/project2.png"
+import pro3 from "../assets/Screenshot (165).png"
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title:"Ecommerce Website Design",
+        text:"",
+        view:"",
+    },
+    {
+        imgsrc: pro2,
+        title:"TextToSpeech",
+        text:"",
+        view:"",
+    },
+    {
+        imgsrc: pro3,
+        title:"RaceMania",
+        text:"",
+        view:"",
+    },
+];
+
+export default ProjectCardData;
